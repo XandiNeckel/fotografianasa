91df02612eb2b111857b65135563afeed1cd6d6d
XandiNeckel <barra_xandi@hotmail.com>
2022-04-14 00:39:45 -0300

    Finalizado e corrigido

--- JS/API.js
+++ JS/API.js
@@ -1,26 +1,26 @@
 const date = $('#date');
-const apiKey = 'wTdSXpJVQF0U208334axP1hunACeVUwdmGajai9L'
+const apiKey = 'wTdSXpJVQF0U208334axP1hunACeVUwdmGajai9L' // chave key do APOD
 
-$('#btn').click(function(){
+$('#btn').click(function () {
     $.ajax({
         url: `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date.val()}`,
         method: 'GET',
 
-        success: function(resposta){
-            $('#resp').html(function(){
-                if (resposta.media_type === 'image'){
+        success: function (resposta) {
+            $('#resp').html(function () {
+                if (resposta.media_type === 'image') { // class atribuida para chamar no css
                     return `<img class='imagem' src='${resposta.url}'/>`
-                }else{
+                } else {
                     return `<iframe class='video' src="${resposta.url}" frameborder="20px"></iframe>`
                 }
             })
         },
-       error: function(erro){
-           console.error(erro)
-       }
+        error: function (erro) {
+            console.error(erro)
+        }
     })
 })
 
-$('form').submit(function(event){
+$('form').submit(function (event) {
     event.preventDefault()
 })
